045ac4bda3483a56faad520f9e9f7ac42958ea5b
Wellington Henrique <wellingtonhlc@gmail.com>
2023-02-22 22:38:12 -0300

    Implementação de rotas

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import { useState } from 'react';
+import { Outlet } from 'react-router-dom';
 import './App.css'
 
 function App() {
   return (
     <div className="App">
       <h1>GitHub Finder</h1>
+      <Outlet/>
     </div>
   )
 }
