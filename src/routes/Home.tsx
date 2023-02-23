eadcdfa83adaa955cb1f97626742c31cdae79027
Wellington Henrique <wellingtonhlc@gmail.com>
2023-02-22 22:59:01 -0300

    Implementação de estrutura de requisição fetch

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,7 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Search } from '../components/Search'
+
+import { UserProps } from '../types/user';
 
 export const Home = () => {
+  const [user, setUser] = useState<UserProps | null>(null);
+
+  const loadUser = async (userName: string) => {
+    const res = await fetch(`https://api.github.com/users/${userName}`);
+    const data = await res.json();
+    console.log(res)
+  }
+
   return (
-    <div>Home</div>
+    <div>
+      <Search loadUser={loadUser}/>
+    </div>
   )
 }
