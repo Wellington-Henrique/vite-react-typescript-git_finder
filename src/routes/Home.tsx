b65042069fcdec228c8165ea2a72cf9f5f310910
Wellington Henrique <wellingtonhlc@gmail.com>
2023-02-23 21:07:08 -0300

    Implementação de componente de erro

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import { Error } from '../components/Error';
 import { Search } from '../components/Search'
 import { User } from '../components/User';
 
@@ -6,10 +7,19 @@ import { UserProps } from '../types/user';
 
 export const Home = () => {
   const [user, setUser] = useState<UserProps | null>(null);
+  const [error, setError] = useState(false);
 
   const loadUser = async (userName: string) => {
+    setError(false);
+    setUser(null);
+    
     const res = await fetch(`https://api.github.com/users/${userName}`);
     const data = await res.json();
+
+    if (res.status === 404){
+      setError(true);
+      return;
+    }
 
     const { avatar_url, login, location, followers, following} = data;
     
@@ -28,6 +38,7 @@ export const Home = () => {
     <div>
       <Search loadUser={loadUser}/>
       {user && <User {...user}/>}
+      {error && <Error/>}
     </div>
   )
 }
